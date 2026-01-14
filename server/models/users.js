58a98c856c19ef3c7c6753531682a3f6d846cfbe
Pratinav Kothia <111730552+pratinavkth@users.noreply.github.com>
2026-01-14 23:24:05 +0530

    created the basic file structure

---
+++ server/models/users.js
@@ -0,0 +1,24 @@
+const { default: mongoose } = require("mongoose");
+
+const userSchema = mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    profilePic:{
+        type:String,
+        required:true,
+    }
+});
+
+const User = mongoose.model("User",userSchema);
+module.exports = User;
